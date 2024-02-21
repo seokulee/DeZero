4fc7b36e4d31e78a05fcc1e555f40eb0df0a55e1
seokulee <cd1004s@gmail.com>
2024-02-21 19:57:49 +0900

    Taylor series, Gradient descent

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -1,8 +1,21 @@
 import numpy as np
 import weakref
+import contextlib
 
 class Config:
     enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 class Variable:
     __array_priority__ = 200
@@ -11,7 +24,6 @@ class Variable:
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f'{type(data)} is not supported')
-                # raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
         self.name = name
@@ -43,7 +55,7 @@ class Variable:
             gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
-                gxs = (gxs, )
+                gxs = (gxs,)
 
             for x, gx in zip(f.inputs, gxs):
                 if x.grad is None:
@@ -105,7 +117,7 @@ class Function:
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys, tuple):
-            ys = (ys, )
+            ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
         if Config.enable_backprop:
@@ -137,7 +149,7 @@ class Mul(Function):
         return y
 
     def backward(self, gy):
-        x0, x1 = self.inputs
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
         return gy * x1, gy * x0
 
 class Neg(Function):
@@ -161,7 +173,7 @@ class Div(Function):
         return y
 
     def backward(self, gy):
-        x0, x1 = self.inputs
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1 ** 2)
         return gx0, gx1
@@ -171,27 +183,14 @@ class Pow(Function):
         self.c = c
 
     def forward(self, x):
-        return x ** self.c
+        y = x ** self.c
+        return y
 
     def backward(self, gy):
         x = self.inputs[0].data
         c = self.c
         gx = c * x ** (c - 1) * gy
         return gx
-
-import contextlib
-
-@contextlib.contextmanager
-def using_config(name, value):
-    old_value = getattr(Config, name)
-    setattr(Config, name, value)
-    try:
-        yield
-    finally:
-        setattr(Config, name, old_value)
-
-def no_grad():
-    return using_config('enable_backprop', False)
 
 def as_array(x):
     if np.isscalar(x):
@@ -212,7 +211,6 @@ def mul(x0, x1):
     return Mul()(x0, x1)
 
 def neg(x):
-    x = as_array(x)
     return Neg()(x)
 
 def sub(x0, x1):
