1803e1b13549a8e3a8db21fb69d311d3b61d1242
seokulee <cd1004s@gmail.com>
2024-02-26 15:45:24 +0900

    MLP

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -12,6 +12,7 @@ else:
     from dezero.core import Variable
     from dezero.core import Parameter
     from dezero.core import Function
+    from dezero.models import Model
     from dezero.core import using_config
     from dezero.core import no_grad
     from dezero.core import as_array
--- dezero/layers.py
+++ dezero/layers.py
@@ -25,12 +25,12 @@ class Layer:
 
     def params(self):
         for name in self._params:
-            yield self.__dict__[name]
+            obj =  self.__dict__[name]
 
-            # if isinstance(obj, Layer):
-            #     yield from obj.params()
-            # else:
-            #     yield obj
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
 
     def cleargrads(self):
         for param in self.params():
